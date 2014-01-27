ce0c01f95090389f8e39af2d8e6eee6aa8dca719
Joel Binnquist <joel.binnquist@gmail.com>
2014-01-28 00:03:04 +0100

    Implement jersey server with JSON support.

--- restresources/src/main/java/se/joelabs/mediaconverter/rest/resources/MoviesResource.java
+++ restresources/src/main/java/se/joelabs/mediaconverter/rest/resources/MoviesResource.java
@@ -1,21 +1,26 @@
 package se.joelabs.mediaconverter.rest.resources;
 
+import com.sun.xml.txw2.annotation.XmlAttribute;
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.logging.Logger;
@@ -23,18 +28,41 @@ import java.util.logging.Logger;
 
 @Path("/movies")
 public class MoviesResource {
+    @XmlRootElement
     public static class Movie implements Serializable {
+        @javax.xml.bind.annotation.XmlAttribute
+        String file;
+        @javax.xml.bind.annotation.XmlAttribute
+        String title;
+        @javax.xml.bind.annotation.XmlAttribute
+        String imdbId;
+        @javax.xml.bind.annotation.XmlAttribute
+        String image;
+        @javax.xml.bind.annotation.XmlAttribute
+        Long id;
 
+        protected Movie() {
+        }
 
+        public Movie(String file, String title, String imdbId, String image, Long id) {
+            this.file = file;
+            this.title = title;
+            this.imdbId = imdbId;
+            this.image = image;
+            this.id = id;
+        }
     }
+
     private static final Logger logger = Logger.getLogger(MoviesResource.class.getName());
-    private ConcurrentNavigableMap<String, List<Movie>> movies;
+    private ConcurrentNavigableMap<Long, Movie> movies;
+    private ConcurrentNavigableMap<String, String> metadata;
     private DB db;
 
     @PostConstruct
     protected void init() {
         db = MConvDB.instance().getDB();
         movies = db.getTreeMap("movies");
+        metadata = db.getTreeMap("metadata");
         logger.info("Initialized DB for ConfigResource");
     }
 
@@ -45,23 +73,24 @@ public class MoviesResource {
 
     @PUT
     @Path("/")
-    public Response setOutputDir(Movie movie) {
-        movies.put(movie.getId(), movie);
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response updateMovie(Movie movie) {
+        if (movie.id == null) {
+            Long nextId = Long.parseLong(metadata.get("nextId") != null ? metadata.get("nextId") : "1");
+            movie.id = nextId;
+            metadata.put("nextId", (nextId + 1) + "");
+        }
+        movies.put(movie.id, movie);
         db.commit();
-        logger.fine("Saved outputdir=" + dirName);
-        return Response.ok().build();
+        return Response.ok(movie.id).build();
     }
 
     @GET
     @Path("/")
-    @Produces("application/json")
-    public String getOutputDir() {
-        List<String> dir = configuration.get("outputdir");
-        logger.fine("outputdir=" + dir);
-        if (dir != null && dir.size() == 1) {
-            return dir.get(0);
-        } else {
-            return null;
-        }
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public List<Movie> getMovies() {
+        List<Movie> movieList = new ArrayList<>(movies.values());
+        logger.fine("movieList=" + movieList);
+        return movieList;
     }
 }
--- restserver/src/main/java/se/joelabs/restserver/Main.java
+++ restserver/src/main/java/se/joelabs/restserver/Main.java
@@ -3,14 +3,12 @@ package se.joelabs.restserver;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
-import se.joelabs.mediaconverter.rest.resources.PingResource;
 
 import java.io.IOException;
 import java.net.URI;
 
 /**
  * Main class.
- *
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
@@ -18,13 +16,14 @@ public class Main {
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     *
      * @return Grizzly HTTP server.
      */
     public static HttpServer startServer() {
-        PingResource pingResource = new PingResource();
         // create a resource config that scans for JAX-RS resources and providers
         // in se.joelabs.restserver package
-        final ResourceConfig rc = new ResourceConfig().packages("se.joelabs.mediaconverter.rest.resources");
+        final ResourceConfig rc = new ResourceConfig()
+                .packages("se.joelabs.mediaconverter.rest.resources");
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
@@ -33,13 +32,13 @@ public class Main {
 
     /**
      * Main method.
-     * @param args
+     *
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+                                         + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
         System.in.read();
         server.shutdownNow();
     }
